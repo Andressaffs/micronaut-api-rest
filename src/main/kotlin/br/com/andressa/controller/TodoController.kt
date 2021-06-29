32e18c323c2f6a0bb476a80cdb16284a80425ee1
Andressa Ferreira <andressaffss@hotmail.com>
2021-06-29 18:36:51 -0300

    crud

--- src/main/kotlin/br/com/andressa/controller/TodoController.kt
+++ src/main/kotlin/br/com/andressa/controller/TodoController.kt
@@ -2,10 +2,10 @@ package br.com.andressa.controller
 
 import br.com.andressa.model.Todo
 import br.com.andressa.repository.TodoRepository
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.Post
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.*
 import javax.inject.Inject
+import javax.validation.Valid
 
 @Controller ("/todos")
 class TodoController(private val todoRepository: TodoRepository) {
@@ -16,7 +16,19 @@ class TodoController(private val todoRepository: TodoRepository) {
     }
 
     @Post
-    fun addTodo (todo: Todo): Todo {
-        return todoRepository.save(todo)
+    fun addTodo (todo: Todo): HttpResponse<Todo> {
+       return HttpResponse.created(todoRepository.save(todo))
     }
+
+    @Put ("/{id}")
+    fun updateTodo(@PathVariable("id") id: Long, todo: Todo): HttpResponse<Todo> {
+        return HttpResponse.ok(todoRepository.update(todo))
+    }
+
+    @Delete ("/{id}")
+    fun deleteTodo (@PathVariable("id") id: Long): HttpResponse<Todo> {
+        todoRepository.deleteById(id)
+        return HttpResponse.noContent()
+    }
+
 }
